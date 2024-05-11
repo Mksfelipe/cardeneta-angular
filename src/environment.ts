006e5e0cb232f4941a9c47f30d6177b942309745
Felipe Sousa <sousafelipe123@gmail.com>
2024-05-11 16:19:16 -0300

    URL

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://cardeneta-web-production.up.railway.app/' // Exemplo de URL da API para o ambiente de desenvolvimento
+    apiUrl: 'https://cardeneta-web-production.up.railway.app' // Exemplo de URL da API para o ambiente de desenvolvimento
 };
 
 
